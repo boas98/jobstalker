c9a14878728497b722b9611ad92276368a449dfc
revze <revando@outlook.com>
2015-11-12 07:47:44 +0700

    Create post page

---
+++ resources/views/user/postproject.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Post a project - Freelancer</title>
+@include('user.lib')
+</head>
+<body class="grey lighten-3">
+@include('user.menu')
+      <div class="recomcon">
+        <div class="row">
+          <div class="col s12 m8">
+            This is post project area
+          </div>
+          @include('user.sidebar')
+        </div>
+      </div>
+</body>
+</html>
---
+++ resources/views/user/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="col s12 m4">
+  <div class="col s12 m12">
+    <div class="card-panel" style="overflow:hidden">
+      <h5 class="light">Revando</h5>
+      <img src="{{ url('assets/pict/2.jpg') }}" style="width:100%; height:auto">
+    </div>
+  </div>
+  <div class="col s12 m12">
+    <div class="card-panel" style="overflow:hidden">
+      <h5 class="light">My funds</h5>
+    </div>
+  </div>
+  <div class="col s12 m12">
+    <div class="card-panel" style="overflow:hidden">
+      <h5 class="light">Rate project</h5>
+    </div>
+  </div>
+</div>
