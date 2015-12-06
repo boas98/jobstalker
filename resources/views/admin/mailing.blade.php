ba3b9c237866de946740c0b3cf0ae35b71759e03
Boas Aditya Christian <boas.chris98@gmail.com>
2015-12-06 22:44:55 +0700

    Fixed mailer and delete some folder

--- resources/views/admin/mailing.blade.php
+++ resources/views/admin/mailing.blade.php
@@ -3,11 +3,21 @@
 <head>
 	<title></title>
 </head>
-<body style="font-family:'Helvetica'; font-size:17px">
-Hello {{ $name }}
-<br>
-Click this link to verify your Jobstalker account
-<br>
-<a href="{{ url('user'.'/'.$username.'/'.'verify'.'/'.$token) }}">Click this link</a>
+<body>
+<section class="content">
+	<div class="row">
+	<div class="cool-md-4">
+	<div class="box box-info">
+	<div class="box-header with-border">
+	<span style="font-size: 25px;"><i class="fa fa-user"></i> Hello, {{$name}}!</span><br>
+	<span>Click this link to verify your Jobstalker account</span>
+	</div>
+	<div class="box-footer">
+		<a href="{{ url('user'.'/'.$username.'/'.'verify'.'/'.$token) }}">Click this link</a>
+	</div>
+	</div>	
+	</div>	
+	</div>
+</section>
 </body>
 </html>
